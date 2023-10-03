74978ff027249acd7969423aef2f6cf1e9767e19
SK0RPIA <contact@skorpia.fr>
2023-10-03 11:45:29 +0200

    feat: :rocket: New init for Game

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,18 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
+
+import fr.caensup.pop.niveau.Map;
+import fr.caensup.pop.personnage.Joueur;
+
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
-
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    	Joueur player = new Joueur("Prince", 5, 1);
+    	Map map01 = new Map("Zone de départ", 4, 20);
+    	
+    	
+    	
+    	
+       
     }
 }

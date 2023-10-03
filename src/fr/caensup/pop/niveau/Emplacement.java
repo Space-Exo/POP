fb9ec249d97af91dc77c5737f1c5922ea2d96659
Space-exo <afarietti@gmail.com>
2023-10-03 11:48:44 +0200

    add emplacement piege && fiolet

--- src/fr/caensup/pop/niveau/Emplacement.java
+++ src/fr/caensup/pop/niveau/Emplacement.java
@@ -1,12 +1,17 @@
 package fr.caensup.pop.niveau;
 
 import fr.caensup.pop.personnage.Personnage;
-
+import fr.caensup.pop.object.Fiole;
+import fr.caensup.pop.niveau.Piege;
 public class Emplacement {
     private Personnage personnage;
+    private Piege piege;
+    private Fiole fiole;
 
     public Emplacement() {
         this.personnage = null;
+        this.piege = null;
+        this.fiole = null;
     }
 
     public Personnage getPersonnage() {
@@ -19,5 +24,29 @@ public class Emplacement {
 
     public void retirerPersonnage() {
         this.personnage = null;
+    }
+
+    public Piege getPiege() {
+        return piege;
+    }
+
+    public void placerPiege(Piege piege) {
+        this.piege = piege;
+    }
+
+    public void retirerPiege() {
+        this.piege = null;
+    }
+
+    public Fiole getFiole() {
+        return fiole;
+    }
+
+    public void placerFiole(Fiole fiole) {
+        this.fiole = fiole;
+    }
+
+    public void retirerFiole() {
+        this.fiole = null;
     }
 }
--- src/fr/caensup/pop/niveau/Piege.java
+++ src/fr/caensup/pop/niveau/Piege.java
@@ -1,4 +1,21 @@
 package fr.caensup.pop.niveau;
 
 public class Piege {
+    private boolean actif;
+
+    public Piege() {
+        this.actif = false; // Par défaut, le piège est inactif
+    }
+
+    public boolean isActif() {
+        return actif;
+    }
+
+    public void activerPiege() {
+        this.actif = true;
+    }
+
+    public void desactiverPiege() {
+        this.actif = false;
+    }
 }
